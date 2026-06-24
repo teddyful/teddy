0f99f5e93d55ebd4836ea63c91a1b0a4e3f3d337
jillurquddus <jillur.quddus@hyperlearning.ai>
2026-06-24 09:34:07 +0100

    Optimise template builder

--- system/src/services/template-builder.js
+++ system/src/services/template-builder.js
@@ -5,9 +5,13 @@
  * @since  0.0.1
  */
 
+import path from 'path';
 import gulp from 'gulp';
 import mustache from 'gulp-mustache';
 
+const DIR_LANGUAGES = 'languages';
+const DIR_TEMPLATES = 'templates';
+const EXT_HTML = 'html';
 
 class TemplateBuilder {
 
@@ -15,32 +19,54 @@ class TemplateBuilder {
         this.config = config;
     }
 
+    #getLanguageDataFilePath(language) {
+        return path.join(
+            this.config.build.distDirs.build,
+            DIR_LANGUAGES,
+            `${language}.json`
+        );
+    }
+
+    #getOutputHtmlDirPath(language) {
+        return path.join(
+            this.config.build.distDirs.build,
+            DIR_TEMPLATES,
+            language
+        );
+    }
+
+    #htmlTemplatesGlob(templatesDirAbsPath) {
+        return `${templatesDirAbsPath.replace(/\\/g, '/')}/*.${EXT_HTML}`;
+    }
+
     async translateTemplates() {
         if ( !this.config.build.opts.ignoreHtml ) {
+
+            // Identify the path to the templates directory.
+            const templatesDirAbsPath = path.join(
+                this.config.system.themes, 
+                this.config.site.theme.name, 
+                DIR_TEMPLATES);
+
             for ( const language of this.config.site.languages.enabled ) {
                 
-                // Identify the path to the templates directory.
-                const templatesDirAbsPath = 
-                    this.config.system.themes + 
-                        '/' + `${this.config.site.theme.name}/templates`;
-
                 // Identify the path to the language data for this language.
-                const languageDataFileAbsPath = 
-                    this.config.build.distDirs.build + 
-                        `/languages/${language}.json`;
+                const languageDataFileAbsPath = this.#getLanguageDataFilePath(
+                    language);
 
                 // Identify the path for the output HTML.
-                const outputHtmlDirAbsPath = this.config.build.distDirs.build 
-                    + `/templates/${language}`;
+                const outputHtmlDirAbsPath = this.#getOutputHtmlDirPath(
+                    language);
 
                 // Inject the language data into the templates and generate
                 // the output HTML.
                 await new Promise((resolve, reject) => {
-                    gulp.src([`${templatesDirAbsPath}/*.html`])
+                    gulp.src([this.#htmlTemplatesGlob(templatesDirAbsPath)])
                         .pipe(mustache(languageDataFileAbsPath, {}, {}))
                         .pipe(mustache(languageDataFileAbsPath, {}, {}))
                         .pipe(gulp.dest(outputHtmlDirAbsPath))
-                        .on('end', resolve);
+                        .on('end', resolve)
+                        .on('error', reject);
                 });
     
             }
